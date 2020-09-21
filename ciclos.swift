89cde8b0ed653a80ff871029da75ab2c1562069f
Deni <denilorenaalfaro@hotmail.com>
2020-09-21 15:13:57 -0700

    swift

---
+++ ciclos.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+var comidas = ["Pollo frito", "Sushi", "Tortas", "Carnita"]
+//print(comidas)
+
+var nombre : String = "Gloria"
+
+var numeros : [Int] = []
+numeros.append(3)
+numeros.append(5)
+
+//print(numeros)
+
+//print("\(nombre) gusta de comer \(comidas[1])")
+
+for comida in comidas {
+   // print("\(nombre) gusta de comer \(comida)")
+}
+
+for i in 0..<3 {
+    //print("Numero: \(i)")
+}
+
